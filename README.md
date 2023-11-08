d42fa1178d7c4a0daf01428a33340b0f1428e5ef
Muhammad Zeeshan <iamzeeshan.dev@gmail.com>
2023-11-08 17:24:44 +0500

    Creating two different route using static logic in createServer function:

--- README.md
+++ README.md
@@ -1,19 +1,3 @@
 # E-Commerce-Backend
 
-Nodemon is a utility depended on about 3 million projects, that will monitor for any changes in your source and automatically restart your server. Perfect for development.
-
-Swap nodemon instead of node to run your code, and now your process will automatically restart when your code changes. To install, get Node.js, then from your terminal run:
-
-`npm install -g nodemon`
-
-Features
-Automatic restarting of application.
-Detects default file extension to monitor.
-Default support for node but easy to run any executable, such as python, ruby, make, etc.
-Ignoring specific files or directories.
-Watch specific directories.
-Works with server applications or one time run utilities and REPLs.
-Scriptable through node require statements.
-Open source and available on github.
-
 
--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 // To use the HTTP server and client one must require('node:http').
 const http = require('http');
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+    if (req.url === '/products') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({
+            data: [
+                {
+                    id: 1,
+                    name: "Product 1"
+                },
+                {
+                    id: 2,
+                    name: "Product 2"
+                },
+            ],
+        }));
+        return;
+    } else if (req.url === '/category') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({
+            data: [
+                {
+                    id: 1,
+                    name: "Category 1"
+                },
+                {
+                    id: 2,
+                    name: "Category 2"
+                },
+            ],
+        }));
+        return;
+    }
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+        data: "Hello World",
+    }));
+
+});
 
 server.listen(4000, () => console.log('Successfully Running Sever on Port 4000 ...'))
