97b4a5ff0ee0de47ca2320d1437f922e557bb2e7
Muhammad Zeeshan <iamzeeshan.dev@gmail.com>
2023-11-08 16:11:32 +0500

    Initial Commit:

---
+++ index.js
@@ -0,0 +1,11 @@
+const os = require('os')
+const path = require('path')
+
+console.log("Version", os.version())
+console.log("Free Memory", os.freemem())
+console.log("Free Memory Home", os.homedir())
+console.log("Free Memory Machine", os.platform())
+
+console.log("Path", path.dirname('/user/dell'))
+
+console.log("Welcome to E-Commerce AppLication", os.freemem())
